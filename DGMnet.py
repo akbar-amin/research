e128d91ec5374a27d7e0ec1002b99e4c0c0287e6
Akbar Amin <aqamin@uci.edu>
2021-01-08 23:13:15 -0800

    first commit

---
+++ DGMnet.py
@@ -0,0 +1,172 @@
+import tensorflow as tf 
+
+
+class Minibatches:
+
+    def __init__(self, nBoundary, nTerminal, dimensions, T_bounds, X_bounds):
+
+        """ Minibatch iterator for time and price points. 
+        
+        Args:
+            nBoundary, nTerminal (float): minibatch-sizes for testing the boundary and terminal conditions, respectively 
+            dimensions (int): the dimensionality of both minibatches
+            T_bounds (tuple, list): lower and upper time boundaries; ex: [1e-10, 1]
+            X_bounds (tuple, list): lower and upper price boundaries; ex: [1e-10, 75]
+        
+        Note: While points are created separately, pairs of points are stacked within the model. 
+
+        Returns:
+            A minibatch with time and price points sampled within the given boundaries, and 
+                another minibatch containing time points sampled at T = 0 and price points 
+                within the given boundaries 
+        """
+        
+        self.nBoundary  = int(nBoundary/2)
+        self.nTerminal = int(nTerminal/2)
+        self.dimensions = dimensions
+
+        self.T_min = T_bounds[0]
+        self.T_max = T_bounds[1]
+        self.X_min = X_bounds[0]
+        self.X_max = X_bounds[1]
+
+    def __iter__(self):
+        return self 
+
+    def __next__(self):
+        
+        t_boundary = tf.Variable(tf.random.uniform((self.nBoundary, self.dimensions), self.T_min, self.T_max))
+        x_boundary = tf.Variable(tf.random.uniform((self.nBoundary, self.dimensions), self.X_min, self.X_max))
+
+        t_terminal = tf.Variable(tf.ones((self.nTerminal, self.dimensions)))
+        x_terminal = tf.Variable(tf.random.uniform((self.nTerminal, self.dimensions), self.X_min, self.X_max))
+
+        return t_boundary, x_boundary, t_terminal, x_terminal
+
+class HiddenDGM(tf.keras.layers.Layer):
+
+    def __init__(self, units, dimensions, active1, active2, **kwargs):
+        """ An itermediate LSTM-like layer used in the DGM neural network. 
+
+        Args:
+            units (int): the number of nodes in the layer (width)
+            dimensions (int): expected dimensions of the input
+            active1 (str): the base activation function for all sublayers except H
+            active2 (str): the activation function for the H-sublayer
+        
+        """
+
+        super(HiddenDGM, self).__init__(**kwargs)
+
+        self.units = units 
+        self.dimensions = dimensions 
+        self.active1 =  tf.keras.activations.get(active1)
+        self.active2 =  tf.keras.activations.get(active2) 
+
+        self.Wz = self.add_weight("Wz", (self.units, self.units), initializer = "glorot_uniform")
+        self.Wg = self.add_weight("Wg", (self.units, self.units), initializer = "glorot_uniform")
+        self.Wr = self.add_weight("Wr", (self.units, self.units), initializer = "glorot_uniform")
+        self.Wh = self.add_weight("Wh", (self.units, self.units), initializer = "glorot_uniform")
+
+        self.Uz = self.add_weight("Uz", (self.dimensions, self.units), initializer = "glorot_uniform")
+        self.Ug = self.add_weight("Ug", (self.dimensions, self.units), initializer = "glorot_uniform")
+        self.Ur = self.add_weight("Ur", (self.dimensions, self.units), initializer = "glorot_uniform")
+        self.Uh = self.add_weight("Uh", (self.dimensions, self.units), initializer = "glorot_uniform")
+
+        self.bz = self.add_weight("bz", (self.units,), initializer = "zeros")
+        self.bg = self.add_weight("bg", (self.units,), initializer = "zeros")
+        self.br = self.add_weight("br", (self.units,), initializer = "zeros")
+        self.bh = self.add_weight("bh", (self.units,), initializer = "zeros")
+
+    def call(self, S, C):
+        """ Runs previous and original tensors through a series of computations within each sublayer 
+
+        Args: 
+            S (tf.Tensor): the tensor being evaluated; contains the previous layer's results
+            C (tf.Tensor): the original input tensor; contains points (t, x)
+        
+        Returns:
+            S_new (tf.Tensor): the resulting tensor 
+        """
+
+        Z = self.active1((tf.matmul(C, self.Uz) + tf.matmul(S, self.Wz)) + self.bz) 
+        G = self.active1((tf.matmul(C, self.Ug) + tf.matmul(S, self.Wg)) + self.bg) 
+        R = self.active1((tf.matmul(C, self.Ur) + tf.matmul(S, self.Wr)) + self.br) 
+        H = self.active2(tf.matmul(C, self.Uh) + tf.matmul(S * R, self.Wh) + self.bh)
+
+        S_new = ((tf.ones_like(G) - G) * H) + Z * S
+        
+        return S_new
+
+
+class DGM(tf.keras.Model):
+
+    def __init__(self, hidden, units, dimensions, active1 = "tanh", active2 = "tanh", active3 = "linear"):
+        """ Architecture of the DGM neural network. 
+        
+        Args:
+            hidden (int): the number of 'HiddenDGM' layers 
+            units (int): the number of nodes per hidden layer 
+            dimensions (int): expected dimensions of the minibatch 
+            active1 (str): activation for the initial layer and all HiddenDGM sublayers except the H-sublayer
+            active2 (str): activation for the H-sublayer in every HiddenDGM layer
+            active3 (str): activation for the final layer
+
+        Note: The last activation is not required by the DGM architecture. Leave as "linear" for it to be pass-through.
+        """
+
+        super(DGM, self).__init__()
+
+        self.dimensions = dimensions 
+        self.hidden = hidden 
+        self.units = units 
+        self.active1 = active1
+        self.active2 = active2
+        self.active3 = active3
+
+
+        self.input_layer = tf.keras.layers.Dense(self.units, self.active1, name = "Dense_Input")
+
+        self.hidden_layers = [HiddenDGM(self.units, self.dimensions, self.active1, self.active2, 
+                    name = "HiddenDGM_{}".format(i + 1)) for i in range(self.hidden)]
+
+        self.output_layer = tf.keras.layers.Dense(1, self.active3, name = "Dense_Output")
+
+    def call(self, inputs):
+        """ Runs a minibatch of time and price points through the DGM network.
+        
+        Args:
+            t, x (tf.Variable, tf.Tensor): time and price tensors with the same shape
+
+        A tensor (S) is created with the original input (C) from the Dense input layer. 
+        The tensor is individually cycled through every hidden layer, where its output becomes
+            the next hidden layer's input.
+        When the last hidden layer returns, its output is run through the Dense output layer, 
+            and the resulting tensor (S_new) is returned. 
+        """
+
+        C = tf.concat(inputs, axis = 0)
+    
+        S = self.input_layer(C)               # Get the initial "output", and then...
+
+        for lstmlayer in self.hidden_layers:  # for every hidden layer, 
+            S = lstmlayer(S, C)               # pipe the output back in as input,
+
+        S_new = self.output_layer(S)          # and return the final output. 
+        
+        return S_new
+
+
+# generator = Minibatches(5000, 500, 20, [0, 1], [2, 3])
+# a, b, c, d = next(generator)
+# model = DGM(3, 50, 20, active1 = "tanh", active2 = "linear", active3 = "linear")
+
+# h = tf.Variable(tf.random.uniform(shape = (5000, 1)))
+# g = model([a, b])
+
+
+#print(tf.squeeze(g))
+#j = tf.expand_dims()
+# h = tf.squeeze(g)
+# print(h)
+#print((tf.transpose(g, perm = [1,0,2])))
---
+++ training.py
@@ -0,0 +1,160 @@
+import os; os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import csv 
+import tensorflow as tf
+from DGMnet import DGM, Minibatches
+
+# References:
+#   [1] Sirignano, J., Spiliopoulos, K., 2018. DGM: A deep learning algorithm for solving partial differential equations. https://arxiv.org/pdf/1708.07469v5.pdf
+#   [2] Al-Aradi, A., 2018. Solving Nonlinear and High-Dimensional Partial Differential Equations via Deep Learning. https://arxiv.org/pdf/1811.08782.pdf
+#   [3] Chen, J., Du, R., Wu, K., 2020. A Comparison Study of Deep Galerkin Method and Deep Ritz Method for Elliptic Problems with Different Boundary Conditions. https://arxiv.org/pdf/2005.04554.pdf
+
+
+
+K = 1         # strike price 
+T = 2         # time-to-expiration (years)
+X0 = 1        # spot price
+sigma = 0.55  # underlying volatility  
+r = 0         # risk-free interest rate
+
+outfile = "C:/Users/akbar/projects/models/DGM"  # for saving the model
+
+
+def AdamLearningSchedule(iteration):
+    """ The Adam optimizer learning rate schedule found most effective in [1]. """
+
+    if iteration <= 5000:
+        LR = 1e-4 
+    elif iteration <= 10000:
+        LR = 5e-4
+    elif iteration <= 20000:
+        LR = 1e-5 
+    elif iteration <= 30000:
+        LR = 5e-6
+    elif iteration <= 40000:
+        LR = 1e-6
+    elif iteration <= 45000:
+        LR = 5e-7
+    else:
+        LR = 1e-7
+
+    return LR 
+
+def train(model, generator, optimizer, iterations, steps):
+    """ Trains the DGM model based on the procedure described in [2].
+
+        Args:
+            model (tf.keras.Model): an instance of 'DGM' or compatible tensorflow/keras model
+            generator (Iterator[tf.Tensor]): an instance of 'Minibatches' or compatible iterator
+            optimizer (tf.optimizers.Optimizer): a tensorflow/keras optimizer 
+            iterations (int): the total number of training cycles 
+            steps (int): the number of SGD steps to take per training cycle 
+
+        Note: The last two arguments are similar to tensorflow's 'epochs' and 'steps_per_epoch' except 
+                a minibatch is repeated across all 'steps_per_epoch'
+
+        Metrics:
+            L1 (tf.Tensor): PDE loss
+            L2 (tf.Tensor): Minimized L2 error
+            L3 (tf.Tensor): Boundary condition Loss
+            L4 (tf.Tensor): Terminal condition loss
+            loss (tf.Tensor): sum of the losses described above
+
+        For every iteration, a minibatch pair will be sampled.
+        Then for every step, the minibatches will be run through the training procedure (i.e. take a descent step). 
+    """
+
+    losses = []
+    
+    for i in range(1, iterations + 1):
+
+        t_boundary, x_boundary, t_terminal, x_terminal = next(generator)
+
+        for _ in range(steps):
+            
+            trainables = model.trainable_variables
+
+            # tapeSGD will take the SGD step 
+            with tf.GradientTape() as tapeSGD:
+                tapeSGD.watch(trainables)
+
+                # tapeD1 and tapeD2 take the first and second derivatives 
+                with tf.GradientTape() as tapeD2:
+                    tapeD2.watch([t_boundary, x_boundary])
+
+                    with tf.GradientTape(persistent = True) as tapeD1:
+                        tapeD1.watch([t_boundary, x_boundary])
+
+                        V = model([t_boundary, x_boundary]) # DGM option value
+                        P = model([t_terminal, x_terminal]) # DGM option payoff
+                        
+
+                    V_t = tapeD1.gradient(V, t_boundary)
+                    V_x = tapeD1.gradient(V, x_boundary)
+                    
+                V_xx = tapeD2.gradient(V_x, x_boundary)
+                #print(V_xx[1])
+
+                V_diff = V_t + 0.5 * sigma**2 * x_boundary**2 * V_xx + r * x_boundary * V_x - r * V
+                # a = tf.expand_dims(V_diff, 1)
+                # b = tf.expand_dims(V, 1)
+                # print(a - b)
+                P_boundary = tf.nn.relu(K - x_boundary)
+                L1 = tf.reduce_mean(tf.square(V_diff * (V - P_boundary)))
+
+                V_diff_min = tf.nn.relu(V_diff)
+                L2 = tf.reduce_mean(tf.square(V_diff_min))
+ 
+                V_min = tf.nn.relu(-(V - P_boundary))
+                L3 = tf.reduce_mean(tf.square(V_min))
+
+                P_terminal = tf.nn.relu(K - x_terminal)
+                L4 = tf.reduce_mean(tf.square(P - P_terminal))
+
+                loss = L1 + L2 + L3 + L4 
+            
+            gradients = tapeSGD.gradient(loss, trainables)
+            optimizer.apply_gradients(zip(gradients, trainables))
+
+        print(f"Iteration: {i} | Loss: {float(loss):.4f} | L1: {float(L1):.4f} | L2: {float(L2):.4f} | L3: {float(L3):.4f} | L4: {float(L4):.4f}")
+        
+        losses.append(tuple(float(m) for m in [i, loss, L1, L2, L3, L4]))
+        optimizer.learning_rate.assign(AdamLearningSchedule(i))
+
+        if i % 25 == 0:           # save checkpoint every 25 iterations
+            model.save(outfile)
+
+            with open(outfile + "/losses.csv", "a+") as fp:
+                writer = csv.writer(fp, delimiter=",", lineterminator="\n")
+                writer.writerows(losses)
+            
+            losses.clear()
+            print(f"\nSaved information for iterations {i - 25}-{i}\n")
+
+    model.save(outfile)
+
+
+
+
+generator = Minibatches(
+    nBoundary = 20000, 
+    nTerminal = 2000, 
+    dimensions = 2, 
+    T_bounds = [1e-10, T], 
+    X_bounds = [1e-10, X0 * 2.5]
+)
+
+optimizer = tf.optimizers.Adam(AdamLearningSchedule(0))
+
+model = DGM(
+    hidden = 3, 
+    units = 50, 
+    dimensions = 2, 
+    active1 = "tanh", 
+    active2 = "tanh", 
+    active3 = "linear"
+)
+
+train(model = model, generator = generator, optimizer = optimizer, iterations = 100000, steps = 15)
+
+
+# model = tf.keras.models.load_model(outfile, compile = False)  
