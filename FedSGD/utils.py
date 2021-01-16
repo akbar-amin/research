62607d9bb2c7b9f53a7e89756ab8a3cf0bc75150
Akbar Amin <akbar.amin917@gmail.com>
2021-01-16 00:25:03 -0800

    update

--- FedSGD/utils.py
+++ FedSGD/utils.py
@@ -48,3 +48,48 @@ def load(outfile, key):
     data = store[key]
 
     return data, metadata
+
+
+def load_by_metric(outfile, metric = "eta"):
+
+    outdir = pathlib.Path("home/projects/models/FedAC/data")
+    store = pd.HDFStore(str(outdir / outfile))
+
+    temp = []
+
+    if metric == "decay":
+        fmtr = "{:.2f}"
+    elif metric == "eta":
+        fmtr = "{:.5f}"
+        
+    for item in store.keys():
+        if metric in item:
+            name = fmtr.format(float(store.get_storer(item).attrs.metadata[metric]))
+            data = store[item]
+            data.name = name
+            temp.append(data)
+
+    df = pd.concat(temp, axis = 1)         
+
+    return df 
+
+def load_runtimes(outfile, metric = "M2", submetric = "B"):
+
+    outdir = pathlib.Path("home/projects/models/FedAC/data")
+    store = pd.HDFStore(str(outdir / outfile))
+
+    temp = {}
+
+    query = "/SUSY_{}_{}".format(metric, submetric)
+    for item in store.keys():
+        if query in item:
+            name = item.replace(query, "")
+            runtime = "{:.5f}".format(float(store.get_storer(item).attrs.metadata["runtime"]))
+            temp.update({name: runtime})
+
+    df = pd.Series(temp)   
+    df = df.astype(float) 
+    df.index = df.index.map(int)    
+    df = df.sort_index()
+
+    return df
